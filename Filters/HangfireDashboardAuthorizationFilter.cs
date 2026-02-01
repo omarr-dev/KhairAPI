d580cabc15eeba017b8d2dfb7a4abea012c712df
اسمك الكامل <omarr9sulaiman@gmail.com>
2026-02-01 16:21:53 +0300

    feat: Simplify Hangfire Dashboard access by allowing all environments

--- Filters/HangfireDashboardAuthorizationFilter.cs
+++ Filters/HangfireDashboardAuthorizationFilter.cs
@@ -11,23 +11,8 @@ namespace KhairAPI
     {
         public bool Authorize(DashboardContext context)
         {
-            var httpContext = context.GetHttpContext();
-            
-            // In development, allow access to the dashboard
-            var env = httpContext.RequestServices.GetService<IWebHostEnvironment>();
-            if (env?.IsDevelopment() == true)
-            {
-                return true;
-            }
-            
-            // In production, require authentication and supervisor role
-            if (!httpContext.User.Identity?.IsAuthenticated ?? true)
-            {
-                return false;
-            }
-            
-            // Check if user is a supervisor
-            return httpContext.User.IsInRole("Supervisor");
+            // Allow access to dashboard in all environments
+            return true;
         }
     }
 }
